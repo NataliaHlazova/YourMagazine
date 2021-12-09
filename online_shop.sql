ccbbc78ae67645ea019f16aa639e112bc5a21be4
NataliaHlazova <87066582+NataliaHlazova@users.noreply.github.com>
2021-12-09 21:10:31 +0200

    YourMagazine

--- online_shop.sql
+++ online_shop.sql
@@ -7,18 +7,4 @@ select * from bucket;
 select * from subscribe;
 select * from product;
 
-
-
-DELETE FROM user WHERE id>=1;
-
 update user set role="ROLE_ADMIN" where id=1;
-
-
-select a.role from user_roles a, users b 
-where b.username="user1" and a.userid=b.userid;
-
-
-
-drop database if exists periodicals;
-create database periodicals char set utf8;
-use periodicals; 
